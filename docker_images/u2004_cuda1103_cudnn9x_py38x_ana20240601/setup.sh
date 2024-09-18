96567d2c0ca3672cdea39e23f699cd30de56f2d2
KhoiDOO <khoido8899@gmai.com>
2024-09-18 10:47:36 +0700

    update docker images

---
+++ docker_images/u2004_cuda1103_cudnn9x_py38x_ana20240601/setup.sh
@@ -0,0 +1,30 @@
+mkdir Download
+cd Download
+
+apt-get update 
+apt-get -y upgrade
+apt-get install -y curl git tmux htop nvtop
+apt-get install zlib1g
+
+apt-get update
+apt-get install -y build-essential software-properties-common
+
+apt install -y make build-essential libssl-dev zlib1g-dev \
+       libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+       libncurses5-dev libncursesw5-dev xz-utils tk-dev
+curl -O https://www.python.org/ftp/python/3.8.20/Python-3.8.20.tgz
+tar xvf Python-3.8.20.tgz
+cd Python-3.8.20
+./configure --enable-optimizations --with-ensurepip=install
+make -j 8
+make altinstall
+
+apt-get update
+apt-get -y install cudnn9-cuda-11
+
+apt-get update
+apt-get -y install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+curl -O https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh
+bash Anaconda3-2024.06-1-Linux-x86_64.sh
+
+cd ..
